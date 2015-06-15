eb1b365ed8b5b365d8683f56f4bcae5f0c6b2721
Nathan Faucett <nathanfaucett@gmail.com>
2015-06-15 12:34:14 -0500

    updates

--- test/test.js
+++ test/test.js
@@ -7,5 +7,6 @@ describe("camelize(string : String[, lowFirstLetter : Boolean])", function() {
         assert.equal(camelize("string-string", false), "StringString");
         assert.equal(camelize("string-string", true), "stringString");
         assert.equal(camelize("string-string"), "stringString");
+        assert.equal(camelize("string.string"), "string.string");
     });
 });
